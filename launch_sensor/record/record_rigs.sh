039668d18a76bc2a48b25c5330e584a49026b3f9
lin <yuyang@udel.edu>
2020-11-21 16:07:18 -0500

    changes to jackal_v2

--- launch_sensor/record/record_rigs.sh
+++ launch_sensor/record/record_rigs.sh
@@ -1,12 +1,17 @@
 source /opt/ros/melodic/setup.bash
 
 rosbag record \
-	/camera/accel/sample \
-	/camera/gyro/sample \
-	/camera/color/image_raw \
-	/camera/depth/image_rect_raw \
 	/camera_pg/image_raw \
-	/imu_ms/data 
+	/gx5/imu/data \
+	/t265/accel/sample \
+	/t265/gyro/sample \
+	/t265/fisheye1/image_raw \
+	/t265/fisheye2/image_raw \
+        /d435i/color/image_raw \
+        /d435i/infra1/image_rect_raw \
+        /d435i/infra2/image_rect_raw \
+        /d435i/accel/sample \
+        /d435i/gyro/sample 
 	
 	
 	
