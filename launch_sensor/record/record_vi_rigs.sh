cdb8f289a00c74050202b63f1597975afee1ca87
lin <yuyang@udel.edu>
2020-11-22 13:06:35 -0500

    changes to launch file

--- launch_sensor/record/record_vi_rigs.sh
+++ launch_sensor/record/record_vi_rigs.sh
@@ -1,17 +1,17 @@
 source /opt/ros/melodic/setup.bash
 
 rosbag record \
-	/T265/accel/sample \
-	/T265/gyro/sample \
-	/T265/fisheye1/image_raw \
-	/T265/fisheye2/image_raw \
-	/D435/color/image_raw \
-	/D435/infra1/image_rect_raw \
-	/D435/infra2/image_rect_raw \
-	/D435/depth/image_rect_raw \
+	/t265/accel/sample \
+	/t265/gyro/sample \
+	/t265/fisheye1/image_raw \
+	/t265/fisheye2/image_raw \
+	/d435/color/image_raw \
+	/d435/infra1/image_rect_raw \
+	/d435/infra2/image_rect_raw \
+	/d435/depth/image_rect_raw \
 	/blackfly/image_raw \
 	/gx3_25/data \
-	/gx3_35/data \
+	/gx3_35/imu/data \
 	/vio/odom \
 	/vicon/vio
 
