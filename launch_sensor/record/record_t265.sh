8050af88c4df12550983c4933585fae42669a6c4
lin <yuyang@udel.edu>
2021-08-20 11:30:48 -0400

    add more launch files

--- launch_sensor/record/record_t265.sh
+++
@@ -1,9 +0,0 @@
-source /opt/ros/melodic/setup.bash
-
-rosbag record \
-	/camera/accel/sample \
-	/camera/gyro/sample \
-	/camera/fisheye1/image_raw \
-	/camera/fisheye2/image_raw \
-	/camera_pg/image_raw \
-	/imu_ms/data
